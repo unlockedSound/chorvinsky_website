d3933ed49327212f50019dea8c42e13bbf8fc19f
David Chorvinsky <firekenace@gmail.com>
2019-04-16 16:38:07 -0400

    major move towards completion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.scss';
-import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
+
+// import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 
 class App extends Component {
     render() {
@@ -8,53 +9,63 @@ class App extends Component {
             <div className="page">
 
                 <section className="title">
-                    <div className="container justify-content-center">
+                    <div className="title__inside container justify-content-center">
                         <div className="title__text">
-                            <div className="title__text__main col-11 offset-1">
+                            <div className="title__text__main col-12">
                                 David Chorvinsky
                             </div>
                             <div className="title__text__subtitle col-auto">
                                 (shifting text here)
                             </div>
                         </div>
-                        <i className="fas fa-angle-down title__arrow"></i>
-                        <i className="fas fa-angle-down title__arrow"></i>
+                        {/*<div className="fas fa-angle-down title__arrow"></div>*/}
+                        <div className="title__arrow">
+                            <div className="title__arrow--large"> Down</div>
+                            <div className="title__arrow--medium"> Down</div>
+                            <div className="title__arrow--small"> Down</div>
+                        </div>
                     </div>
                 </section>
 
-                <section className="row">
-                    <div className="about">
-                        With a deep passion for design and "creating" everything from web applications and
-                        virtual economy analyses to events or calligraphy I find myself always working on
-                        something.
-                        This mix of passions, both digital and physical, lead me to learn about many things and
-                        from
-                        many wonderful people. Through the osmosis of information from so many sources I found
-                        myself
-                        naturally drawn to the role of Product Manager for both my career and personal projects,
-                        working
-                        with teams and friends, to create things we can proud of.
+
+                <section
+                    className="about">
+                    <div
+                        className="about__inside container-fluid">
+                        <div
+                            className="container justify-content-center">
+                            <div
+                                className="about__title"> Day
+                                job:
+                            </div>
+                            <div className="about__text">Product Management with the title of Front End Dev</div>
+                            <div className="about__title"> After hours:</div>
+                            <div className="about__text">Chef, Economic Analyst, Small Business Owner, Event Planner
+                            </div>
+                            <div className="about__title"> Late Night:</div>
+                            <div className="about__text"> Dancer, Event Organizer, Dungeon Master</div>
+                        </div>
                     </div>
                 </section>
 
-                <div className="projects">
-                    <div className="col-12">
-                        Between work and hope I tend to keep myself busy with more than a few projects.
-                    </div>
-                    <div className="projects__dcGov"></div>
+                {/*<div className="projects">*/}
+                {/*    <div className="col-12">*/}
+                {/*        Between work and hope I tend to keep myself busy with more than a few projects.*/}
+                {/*    </div>*/}
+                {/*    <div className="projects__dcGov"></div>*/}
 
-                    <div className="projects__spkez"></div>
-                    <div className="projects__bg"></div>
-                </div>
+                {/*    <div className="projects__spkez"></div>*/}
+                {/*    <div className="projects__bg"></div>*/}
+                {/*</div>*/}
 
-                <section className="row atc">
-                </section>
+                {/*<section className="row atc">*/}
+                {/*</section>*/}
 
-                <section className="row undergrad">
-                </section>
+                {/*<section className="row undergrad">*/}
+                {/*</section>*/}
 
-                <section className="row dreamWizards">
-                </section>
+                {/*<section className="row dreamWizards">*/}
+                {/*</section>*/}
 
             </div>
         );
